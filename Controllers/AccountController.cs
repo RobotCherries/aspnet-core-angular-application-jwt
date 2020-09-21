fc76fccb8c961d2e6a8a21605486c194f3b4b0e9
RobotCherries <zdip14@gmail.com>
2020-09-21 23:59:02 +0300

    Added register task in AccountController

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ng_core_auth.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 // MIUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
@@ -17,6 +21,47 @@ namespace ng_core_auth
         {
             _userManager = userManager;
             _signInManager = signInManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel formData)
+        {
+            // Errors holder
+            List<string> errorsList = new List<string>();
+
+            var user = new IdentityUser
+            {
+                Email = formData.Email,
+                UserName = formData.Username,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var result = await _userManager.CreateAsync(user, formData.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+
+                return Ok(new
+                {
+                    username = user.UserName,
+                    email = user.Email,
+                    status = 1,
+                    message = "Registration Successful"
+                });
+            }
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    errorsList.Add(error.Description);
+                }
+
+                return BadRequest(new JsonResult(errorsList));
+            }
+
+            // TODO: Send Confirmation Email
         }
     }
 }
